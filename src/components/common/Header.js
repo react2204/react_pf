8e2ab0a9df62d5d352f2eb7b0bd2d47af9230188
tubi55 <tubi55@nate.com>
2022-04-11 15:24:05 +0900

    header 스타일 완성

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,7 +3,20 @@ import React from 'react';
 function Header() {
 	return (
 		<header>
-			<h1>header</h1>
+			<div className='inner'>
+				<h1>
+					<a href='#'>LOGO</a>
+				</h1>
+
+				<ul id='gnb'>
+					<li>
+						<a href='#'>Gallery</a>
+					</li>
+					<li>
+						<a href='#'>Youtube</a>
+					</li>
+				</ul>
+			</div>
 		</header>
 	);
 }
