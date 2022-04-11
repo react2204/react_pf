b3437a993c94a5b7906f10c51a206f6b9f2d25f8
tubi55 <tubi55@nate.com>
2022-04-11 14:52:18 +0900

    기본 메인, 서브 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { Route } from 'react-router-dom';
+
 import './scss/style.scss';
+//common 컴포넌트
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+
+//main 컴포넌트
 import Visual from './components/main/Visual';
 import Content from './components/main/Content';
+
+//sub 컴포넌트
+import Youtube from './components/sub/Youtube';
+import Gallery from './components/sub/Gallery';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Visual />
-			<Content />
+
+			<Route exact path='/'>
+				<Visual />
+				<Content />
+			</Route>
+
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/gallery' component={Gallery} />
+
 			<Footer />
 		</>
 	);
