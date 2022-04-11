7c88bf55fa484e979b8d5e5cd0c1967017600f4a
tubi55 <tubi55@nate.com>
2022-04-11 15:34:24 +0900

    NavLink로 메뉴 클릭시 해당 라우터 연결

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+	const active = { color: 'aqua' };
+
 	return (
 		<header>
 			<div className='inner'>
 				<h1>
-					<a href='#'>LOGO</a>
+					<NavLink exact to='/' activeStyle={active}>
+						Logo
+					</NavLink>
 				</h1>
 
 				<ul id='gnb'>
 					<li>
-						<a href='#'>Gallery</a>
+						<NavLink to='/gallery' activeStyle={active}>
+							Gallery
+						</NavLink>
 					</li>
 					<li>
-						<a href='#'>Youtube</a>
+						<NavLink to='/youtube' activeStyle={active}>
+							Youtube
+						</NavLink>
 					</li>
 				</ul>
 			</div>
