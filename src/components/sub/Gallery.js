b8327c47eccba16941d3d2471329e551f449c615
tubi55 <tubi55@nate.com>
2022-04-12 17:55:51 +0900

    styled components로 스타일 적용

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,10 +1,32 @@
 import Layout from '../common/Layout';
 import styled from 'styled-components';
 
+const path = process.env.PUBLIC_URL;
+
+const Box = styled.div`
+	width: 100px;
+	height: 100px;
+	border: 1px solid #333;
+	position: relative;
+
+	&::after {
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		content: 'test';
+		display: block;
+		width: 100%;
+		height: 100%;
+		background-color: aqua;
+		background-image: url(${path}/img/member1.jpg);
+		background-size: cover;
+	}
+`;
+
 function Gallery() {
 	return (
 		<Layout name={'Gallery'}>
-			<div className='box'></div>
+			<Box></Box>
 		</Layout>
 	);
 }
