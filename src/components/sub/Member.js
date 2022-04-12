0efe8bc687bead2b52b6a625c709453d18fd667b
tubi55 <tubi55@nate.com>
2022-04-12 15:44:23 +0900

    axios로 멤버 데이터 출력

--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -1,9 +1,31 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Member() {
+	const path = process.env.PUBLIC_URL;
+	const [members, setMembers] = useState([]);
+
+	useEffect(() => {
+		axios.get(`${path}/DB/member.json`).then((json) => {
+			//console.log(json.data.data);
+			setMembers(json.data.data);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Member'}>
-			<p>멤버 컨텐츠</p>
+			<ul className='memberList'>
+				{members.map((member, idx) => {
+					return (
+						<li key={idx}>
+							<img src={`${path}/img/${member.pic}`} />
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</li>
+					);
+				})}
+			</ul>
 		</Layout>
 	);
 }
