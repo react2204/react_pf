1f40c244f56ac711e2f4c3daa9202c9b8fde1061
tubi55 <tubi55@nate.com>
2022-04-12 12:48:27 +0900

    useEffect로 페이지 컴포넌트 출력시 모션효과적용

--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function Member() {
+	const frame = useRef(null);
+
+	useEffect(() => {
+		console.log('member 컴포넌트 생성');
+		frame.current.classList.remove('on');
+		frame.current.classList.add('on');
+
+		return () => {
+			console.log('member컴포넌트 소멸');
+		};
+	}, []);
+
 	return (
-		<section className='content member'>
+		<section className='content member' ref={frame}>
 			<figure></figure>
 			<div className='inner'>
 				<h1>Member</h1>
