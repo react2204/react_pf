6addaef4a5f5cec2463d6ca3a8c6d2574330e2fd
tubi55 <tubi55@nate.com>
2022-04-13 16:12:24 +0900

    트래픽 정보 on off

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,28 +1,40 @@
-import React, {useRef, useEffect} from 'react'
+import React, { useRef, useEffect, useState } from 'react';
 import Layout from '../common/Layout';
 
 function Location() {
-  //가상돔을 참조할 container객체를 useRef로 생성
-  const container = useRef(null);
-  //window전역객체 안에서 kakao라는 객체를 찾은 다음에 kakao라는 변수이름으로 비구조화 할당
-  const {kakao} = window;
+	const container = useRef(null);
+	const { kakao } = window;
+	//카카오맵 api를 통해서 생성된 인스턴스를 옮겨담을 state추가
+	const [map, setMap] = useState({});
 
-  useEffect(()=>{   
-    const options = { 
-      center: new kakao.maps.LatLng(33.450701, 126.570667), 
-      level: 3 
-    };
+	useEffect(() => {
+		const options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3,
+		};
 
-    //첫번째 인수로 지도가 들어갈 프레임 등록, 두번째 인수로 지도위치, 줌레벨 옵션값 등록해서 인스턴스 생성
-    const map = new kakao.maps.Map(container.current, options); 
-  },[]);
+		const map = new kakao.maps.Map(container.current, options);
+		//인스턴스 map을 state map으로 옮겨담음
+		setMap(map);
+	}, []);
 
-  return (
-    <Layout name={'Location'}>
-      {/* container객체를 해당 가상돔에 참조 */}
-      <div id="map" ref={container}></div>
-    </Layout>
-  )
+	return (
+		<Layout name={'Location'}>
+			<div id='map' ref={container}></div>
+
+      {/* 버튼 클릭시 각각 트래픽정보 on / off */}
+			<button
+				onClick={() => map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)}>
+				Traffic ON
+			</button>
+			<button
+				onClick={() =>
+					map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+				}>
+				Traffic OFF
+			</button>
+		</Layout>
+	);
 }
 
-export default Location
+export default Location;
