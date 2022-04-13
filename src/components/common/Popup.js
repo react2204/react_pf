acebcafd9fba45c1f25706962c154183216e3117
tubi55 <tubi55@nate.com>
2022-04-13 14:10:10 +0900

    popup컴포넌트 styled-components로 스타일링

--- src/components/common/Popup.js
+++ src/components/common/Popup.js
@@ -1,11 +1,45 @@
+import styled from 'styled-components';
+
+const Pop = styled.aside`
+  width: 100%;
+  height: 100vh;
+  padding: 5vw;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  background: rgba(0, 0, 0, 0.9);
+  z-index: 10;
+
+  > span {
+    font: bold 14px/1 'arial';
+    color: #fff;
+    cursor: pointer;
+    position: absolute;
+    top: 3vw;
+    right: 3vw;
+  }
+
+  .con {
+    width: 100%; height: 100%; border: 1px solid #888; overflow: hidden;
+
+    iframe {
+      width: 100%; height: 100%; object-fit: cover;
+    }
+
+    img {
+      width: 100%; height: 1 00%; object-fit: cover;
+    }
+  }
+`
+
 function Popup(props) {
 	return (
-		<aside className='popup'>
+		<Pop>
 			<span onClick={()=>props.setOpen(false)}>close</span>
       <div className="con">
         {props.children}
       </div>
-		</aside>
+		</Pop>
 	);
 }
 
--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,43 +1,10 @@
 import Layout from '../common/Layout';
-import styled from 'styled-components';
-
 const path = process.env.PUBLIC_URL;
-
-const Box = styled.div`
-	width: 100px;
-	height: 100px;
-	border: 1px solid #333;
-	position: relative;
-
-	&::after {
-		position: absolute;
-		top: 0px;
-		left: 0px;
-		content: 'test';
-		display: block;
-		width: 100%;
-		height: 100%;
-		background-color: aqua;
-		background-image: url(${path}/img/member1.jpg);
-		background-size: cover;
-	}
-`;
-
-const LinkBtn = styled.button`
-	display: block;
-	padding: 10px 20px;
-	background: aqua;
-	border: none;
-	border-raius: 5px;
-	color: #fff;
-`
 
 function Gallery() {
 	return (
 		<Layout name={'Gallery'}>
-			<Box></Box>
-			<LinkBtn>버튼</LinkBtn>
-			<div className="box2"></div>
+			
 		</Layout>
 	);
 }
