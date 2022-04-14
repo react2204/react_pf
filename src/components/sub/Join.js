7cf5f499262ea01ee79e90b4fdaf4413f86a3ca2
tubi55 <tubi55@nate.com>
2022-04-14 11:49:44 +0900

    인풋요소에 값을 입력하면 스테이트값이 변경되고 해당 인풋요소에 내용이 출력되는 흐름 설명

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -7,12 +7,12 @@ function Join() {
 	};
 	const [val, setVal] = useState(initVal);
 
-  //인풋요소에 값을 입력할때마다 실행할 함수
-  //해당 입력된 인풋 요소의 name, value값으로 
-  //기존 state값을 변경
+  
   const handleChange = (e) =>{
+    //순서2- 입력하고 있는 인풋 요소의 name, value값 변수에저장
     const {name, value} = e.target;
     console.log(`name: ${name}, value: ${value}`);
+    //순서3 - 기본 val을 복사해서 방급 입력받은 value값으로 덮음
     setVal({...val, [name]: value});
     console.log(val);
   }
@@ -36,7 +36,9 @@ function Join() {
 										id='userid'
 										name='userid'
 										placeholder='아이디를 입력하세요'
-                    value= {val.userid}                    
+                    //순서4 - 변경의 val 스테이트의 userid값을 인풋요소에 출력
+                    value= {val.userid}  
+                    //순서1 - 인풋에 값을 입력하면 handleChange함수 호출
                     onChange={handleChange}
 									/>
 								</td>
