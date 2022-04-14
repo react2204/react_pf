f7e05b3d5263b089926ca0925d1e69a385a53193
tubi55 <tubi55@nate.com>
2022-04-14 11:44:06 +0900

    input요소에 값을 입력할때마다 state를 변경해주고 해당 state값을 input요소에 출력

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,11 +1,58 @@
 import Layout from '../common/Layout';
+import { useState } from 'react';
 
 function Join() {
-  return (
-    <Layout name={'Join'}>
-      <h2>회원가입 페이지</h2>
-    </Layout>
-  )
+	const initVal = {
+		userid: '',
+	};
+	const [val, setVal] = useState(initVal);
+
+  //인풋요소에 값을 입력할때마다 실행할 함수
+  //해당 입력된 인풋 요소의 name, value값으로 
+  //기존 state값을 변경
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+    console.log(`name: ${name}, value: ${value}`);
+    setVal({...val, [name]: value});
+    console.log(val);
+  }
+
+	return (
+		<Layout name={'Join'}>
+			<form>
+				<fieldset>
+					<legend>회원가입 폼 양식</legend>
+
+					<table border='1'>
+						<caption>회원가입 정보입력</caption>
+						<tbody>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>USER ID</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='userid'
+										name='userid'
+										placeholder='아이디를 입력하세요'
+                    value= {val.userid}                    
+                    onChange={handleChange}
+									/>
+								</td>
+							</tr>
+							<tr>
+								<th colSpan='2'>
+									<input type='reset' value='CANCEL' />
+									<input type='submit' value='SEND' />
+								</th>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
+		</Layout>
+	);
 }
 
 export default Join;
