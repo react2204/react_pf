faee464c349ef2fcd1c5b403dce0acd5c7b3f2ec
tubi55 <tubi55@nate.com>
2022-04-14 14:38:30 +0900

    비번 인증 처리

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -4,52 +4,61 @@ import { useState, useEffect } from 'react';
 function Join() {
 	const initVal = {
 		userid: '',
+		pwd1: '',
+		pwd2: '',
 	};
-	const [val, setVal] = useState(initVal); 
-  const [err, setErr] = useState({});
+	const [val, setVal] = useState(initVal);
+	const [err, setErr] = useState({});
 
-  //순서4 - 인수로 state val값을 전달
-  const check = (val) => {
-    //내부적으로 빈 에러 객체를 생성
-    const errs = {};
-    //입력한 val의 값중 유저아이디값 글자갯수가 5보다 적으면
-    if( val.userid.length < 5){
-      //빈 에러객체에 userid키값 만들어서 에러메세지 등록
-      errs.userid = '아이디를 5글자 이상 입력하세요';
-    }
-    
-    //만약 에러상황이 아니면 조건문이 무시되면서
-    //빈 에러객체를 반환하고
-    //만약 에러상황이면  조건문이 실행되면서
-    //에러메세지가 담긴 에러객체 반환
-    return errs;
-  }  
-  
-  const handleChange = (e) =>{
-    const {name, value} = e.target; 
-    setVal({...val, [name]: value});  
-  }
+	const check = (val) => {
+		const errs = {};
+		const eng = /[a-zA-Z]/;
+		const num = /[0-9]/;
+		const spc = /[~!@#$%^&*()(_+)]/;
 
+		if (val.userid.length < 5) {
+			errs.userid = '아이디를 5글자 이상 입력하세요';
+		}
+    //순서5 - 입력 비번1에 특수문자, 영어, 숫자, 5글자이상이 아니면
+    //새로운 에러객체 생성
+		if (
+			val.pwd1 < 5 ||
+			!eng.test(val.pwd1) ||
+			!num.test(val.pwd1) ||
+			!spc.test(val.pwd1)
+		) {
+			errs.pwd1 =
+				'비밀번호는 5글자 이상, 문자, 숫자, 특수문자를 모두 포함하세요';
+		}
+    //순서6 - 비번1, 비번2값이 다르면 에러객체 생성
+		if (val.pwd1 !== val.pwd2 || !val.pwd2) {
+			errs.pwd2 = '두개의 비밀번호를 동일하게 입력하세요.';
+		}
 
-  //순서2 - 해당함수가 호출
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //순서3 - 현재 val의 값을 check함수의 인수로 전달
-    setErr(check(val));
-    //순서5 - 반환된 결과값의 에러객체가
-    //setErr함수에 의해서 err스테이트에 전달
-  }
+		return errs;
+	};
 
-  
-  //순서6 - err스테이트가 의존성에 등록되어 있으므로
-  //err 값이 변경될떄마다 콘솔문 출력
-  useEffect(()=>{
-    console.log(err);
-  },[err])
+  //순서2 - 입력된 값이 val 스테이트에 저장
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...val, [name]: value });
+	};
+
+  //순서3 - 전송버튼 클릭시 해당 함수호출
+	const handleSubmit = (e) => {
+		e.preventDefault();
+    //순서4 - 완성된 state값을 check함수에 인수로 전달
+		setErr(check(val));
+    //순서7- check함수에 의해서 만들어진 에러객체를 err스테이트로 변경
+	};
+
+  //순서8- err 스테이트가 변경될떄마다 콘솔출력
+	useEffect(() => {
+		console.log(err);
+	}, [err]);
 
 	return (
 		<Layout name={'Join'}>
-      {/* 순서1- 전송버튼 눌러서 submit이벤트 발생시 handleSubmit호출 */}
 			<form onSubmit={handleSubmit}>
 				<fieldset>
 					<legend>회원가입 폼 양식</legend>
@@ -66,9 +75,40 @@ function Join() {
 										type='text'
 										id='userid'
 										name='userid'
-										placeholder='아이디를 입력하세요'                  
-                    value= {val.userid}             
-                    onChange={handleChange}
+										placeholder='아이디를 입력하세요'
+										value={val.userid}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+              {/* 순서1- 비번2개를 입력해서 state값에 전달 */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd1'>PASSWORD</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										name='pwd1'
+										id='pwd1'
+										placeholder='비밀번호를 입력하세요'
+										value={val.pwd1}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pwd2'>RE-PASSWORD</label>
+								</th>
+								<td>
+									<input
+										type='password'
+										name='pwd2'
+										id='pwd2'
+										placeholder='비밀번호를 재입력하세요'
+										value={val.pwd2}
+										onChange={handleChange}
 									/>
 								</td>
 							</tr>
