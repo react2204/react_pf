5e4f8aa65229082bed50620f305bc35c1e0dc07d
tubi55 <tubi55@nate.com>
2022-04-15 10:23:38 +0900

    filter를 활용해서 삭제할 순번의 포스트만 제외하고 나머지 포스트를 화면에 출력

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -6,26 +6,39 @@ function Community() {
 	const textarea = useRef(null);
 
 	const dummyPosts = [
+    { title: 'Hello5', content: 'Here comes description in detail.' },
+    { title: 'Hello4', content: 'Here comes description in detail.' },
+    { title: 'Hello3', content: 'Here comes description in detail.' },
 		{ title: 'Hello2', content: 'Here comes description in detail.' },
 		{ title: 'Hello1', content: 'Here comes description in detail.' },
 	];
 	const [posts, setPosts] = useState(dummyPosts);
 
-  //post추가 함수 
-  const createPost=()=>{
-    setPosts([
-      { title: input.current.value, content: textarea.current.value },
-      ...posts,
-    ]);
-    //글 저장후 input창 초기화
-    resetPost();
+  //post입력창 초기화함수
+	const resetPost = () => {
+		input.current.value = '';
+		textarea.current.value = '';
+	};
+
+	//post추가 함수
+	const createPost = () => {
+		setPosts([
+			{ title: input.current.value, content: textarea.current.value },
+			...posts,
+		]);
+		resetPost();
+	};
+
+  //post삭제 함수
+  const deletePost = (index) => {
+    console.log('삭제할 포스트 순번', index);
+
+    setPosts(
+      posts.filter((post, idx)=> idx !== index)
+    )
   }
 
-  //post입력창 초기화함수
-  const resetPost=()=>{
-    input.current.value='';
-    textarea.current.value='';
-  }
+	
 
 	return (
 		<Layout name={'Community'}>
@@ -48,6 +61,11 @@ function Community() {
 						<article key={idx}>
 							<h2>{post.title}</h2>
 							<p>{post.content}</p>
+
+              <div className="btns">
+                {/* 순서1- 삭제버튼 클릭시 삭제포스트의 순서값 전달 */}
+                <button onClick={()=>deletePost(idx)}>delete</button>
+              </div>
 						</article>
 					);
 				})}
