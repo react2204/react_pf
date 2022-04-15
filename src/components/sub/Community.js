c9c9a7b806db40a97758994c314baa7a645fc1c0
tubi55 <tubi55@nate.com>
2022-04-15 09:50:34 +0900

    새로운 게시글 포스트 생성

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,37 +1,43 @@
-import {useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Layout from '../common/Layout';
 
 function Community() {
-  const dummyPosts = [
-    {title: 'Hello1', content: 'Here comes description in detail.'},
-    {title: 'Hello2', content: 'Here comes description in detail.'},
-  ]
-  const [posts, setPosts] = useState(dummyPosts);
+	const input = useRef(null);
+	const textarea = useRef(null);
+
+	const dummyPosts = [
+		{ title: 'Hello2', content: 'Here comes description in detail.' },
+		{ title: 'Hello1', content: 'Here comes description in detail.' },
+	];
+	const [posts, setPosts] = useState(dummyPosts);
 
 	return (
 		<Layout name={'Community'}>
 			<div className='inputBox'>
-				<input type='text' placeholder='제목을 입력하세요' />
+				<input type='text' placeholder='제목을 입력하세요' ref={input} />
 				<br />
 				<textarea
 					cols='30'
 					rows='10'
-					placeholder='본문을 입력하세요.'></textarea>
+					placeholder='본문을 입력하세요.'
+					ref={textarea}></textarea>
 
-          <button>cancel</button>
-          <button>create</button>
+				<button>cancel</button>
+				<button onClick={()=>{
+          setPosts([{title: input.current.value, content: textarea.current.value }, ...posts])
+        }}>create</button>
 			</div>
 
-      <div className="showBox">
-        {posts.map((post,idx)=>{
-          return (
-            <article key={idx}>
-              <h2>{post.title}</h2>
-              <p>{post.content}</p>
-            </article>
-          )
-        })}
-      </div>
+			<div className='showBox'>
+				{posts.map((post, idx) => {
+					return (
+						<article key={idx}>
+							<h2>{post.title}</h2>
+							<p>{post.content}</p>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
