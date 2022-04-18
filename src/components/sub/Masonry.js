308466640419d4707453b1d39027c78263e56da4
tubi55 <tubi55@nate.com>
2022-04-18 10:39:16 +0900

    getFlickr함수에 호출 url을 인수로 넣어서 갤러리 전환효과적용

--- src/components/sub/Masonry.js
+++ src/components/sub/Masonry.js
@@ -5,35 +5,38 @@ import axios from 'axios';
 function Masonry() {
   const key = '89aae050d1d8c006bdb5bf866029199d';
 	const method1 = 'flickr.interestingness.getList';
+  const method2 = 'flickr.photos.search';
 	const num = 20;
-	const url = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
-  //gallery frame을 담을 객체 useRef로 생성
+	const url1 = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
+  const url2 = `https://www.flickr.com/services/rest/?method=${method2}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&tags=ocean`;
   const frame = useRef(null);
-  //flickr데이터를 옮겨담을 items state생성
-  const [items, setItems] = useState([]);
-  
+  const [items, setItems] = useState([]);  
 
-  //async await 구문으로 모든 flickr데이터가 불러온뒤 frame에 on을 붙여서 활성화하는 함수 정의
-  const getFlickr = async () =>{
-    //axios구문으로 모든 데이터가 불러와지면
+  const getFlickr = async (url) =>{  
     await axios.get(url).then((json)=>{
       console.log(json.data.photos.photo);
       setItems(json.data.photos.photo);
-    });
-
-    //동기적으로 frame에 on이 붙어서 화면에 보이는 모션 처리
+    }); 
     frame.current.classList.add('on');        
   }
 
-  //처음 컴포넌트가 생성이 되면 위에 정의한 getFlickr함수 호출
   useEffect(()=>{
-    getFlickr();
+    getFlickr(url1);
   },[]);
-
 
   return (
     <Layout name={'Masonry'}>
-      <div className="frame" ref={frame}>
+      <button onClick={()=>{
+        frame.current.classList.remove('on');
+        getFlickr(url1);
+      }}>interest 갤러리 보기</button>
+      
+      <button onClick={()=>{
+        frame.current.classList.remove('on');
+        getFlickr(url2);
+      }}>ocean 갤러리 보기</button>
+
+      <div className="frame" ref={frame}> 
         {items.map((item,idx)=>{
           return (
             <article key={idx}>
