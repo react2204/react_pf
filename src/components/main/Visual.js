9b47db469a2d623f2c9cefe784897b309a839894
tubi55 <tubi55@nate.com>
2022-04-20 12:15:27 +0900

    메인 비주얼에 swiper연결

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from "swiper";
+import 'swiper/css';
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 function Visual() {
-	return <figure id='visual' className='myScroll'></figure>;
+	return <figure id='visual' className='myScroll'>
+		<Swiper
+			spaceBetween={40}
+			slidesPerView={3}
+			loop={true}
+			pagination={{
+				clickable: true,
+			}}
+			navigation={true}
+			modules={[Pagination, Navigation]}
+		>
+			<SwiperSlide>1</SwiperSlide>
+			<SwiperSlide>2</SwiperSlide>
+			<SwiperSlide>3</SwiperSlide>
+			<SwiperSlide>4</SwiperSlide>
+			<SwiperSlide>5</SwiperSlide>
+		</Swiper>
+	</figure>;
 }
 
 export default Visual;
