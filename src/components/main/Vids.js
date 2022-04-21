6bfde35087ce96834fdce813106bfef97a992c3b
tubi55 <tubi55@nate.com>
2022-04-21 15:11:13 +0900

    메인의 vids컴포넌트에서 미리보기 형식으로 3개의 유튜브 데이터만 출력

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Vids() {
+	const vidData = useSelector((state) => state.youtubeReducer.youtube);
+
 	return (
 		<section id='vids' className='myScroll'>
 			<h1>Recent Youtube</h1>
+			<ul className='vidList'>
+				{vidData.map((vid, idx) => {
+					if (idx < 3)
+						return (
+							<li key={idx}>
+								<img src={vid.snippet.thumbnails.medium.url} />
+							</li>
+						);
+				})}
+			</ul>
 		</section>
 	);
 }
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,11 +1,9 @@
 import { useState, useRef } from 'react';
-//store에 있는 데이터를 가져오기 위한 useSelector import
 import { useSelector } from 'react-redux';
 import Layout from '../common/Layout';
 import Popup from '../common/Popup';
 
 function Youtube() {
-	//해당 컴포넌트 함수 호출시 store로부터 youtube데이터를 useSelector로 가져옴
 	const vidData = useSelector((state) => state.youtubeReducer.youtube);
 	const pop = useRef(null);
 	const [index, setIndex] = useState(0);
@@ -13,7 +11,6 @@ function Youtube() {
 	return (
 		<>
 			<Layout name={'Youtube'}>
-				{/* 가져온 데이터를 반복을 돌면서 리스트 출력 */}
 				{vidData.map((item, idx) => {
 					const desc = item.snippet.description;
 					const date = item.snippet.publishedAt;
@@ -35,7 +32,6 @@ function Youtube() {
 			</Layout>
 
 			<Popup ref={pop}>
-				{/* 해당 vidData값이 있을때 팝업안에 데이터 출력 */}
 				{vidData.length !== 0 && (
 					<iframe
 						src={
