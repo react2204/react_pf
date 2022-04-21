b55414653cb7af4e5e6643ec60bbc1dc950abd30
tubi55 <tubi55@nate.com>
2022-04-21 12:29:25 +0900

    일단은 리듀서로부터 빈배열을 받은 다음 컴포넌트가 실행되면 axios로 받은 데이터를 dispatch를 통해 reducer에 전달해서 다시 화면 출력

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,43 +1,54 @@
 import Layout from '../common/Layout';
 import axios from 'axios';
 import { useEffect, useState, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Popup from '../common/Popup';
+import { setYoutube } from '../../redux/actions';
 
 function Youtube() {
-	const pop = useRef(null);
-	const key = 'AIzaSyBZFBuapkASPcRBXB2-d_ak5-ecCpVicI4';
-	const num = 5;
-	const id = 'PLHtvRFLN5v-UVVpNfWqtgZ6YPs9ZJMWRK';
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&maxResults=${num}&playlistId=${id}`;
+	//store에 youtubeReducer데이터 가져옴 (빈배열)
+	const vidData = useSelector((state) => state.youtubeReducer.youtube);
+	//dispatch전송함수 활성화
+	const dispatch = useDispatch();
 
-	const [items, setItems] = useState([]);	
+	const pop = useRef(null);
 	const [index, setIndex] = useState(0);
-	//데이터 로딩에 관련한 state추가
 	const [loading, setLoading] = useState(false);
 
-	useEffect(() => {	
-		axios.get(url).then((json) => {
-			console.log(json.data.items);
-			setItems(json.data.items);
-			//모든 데이터가 호출되고 state값에 담기면 loding state값 true로 변경
+	//axios 데이터 호출 구문을 함수로 정의
+	const fetchYoutube = async () => {
+		const key = 'AIzaSyBZFBuapkASPcRBXB2-d_ak5-ecCpVicI4';
+		const num = 5;
+		const id = 'PLHtvRFLN5v-UVVpNfWqtgZ6YPs9ZJMWRK';
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&maxResults=${num}&playlistId=${id}`;
+
+		//해당함수에서 axios가 유튜브 데이터를 받아오면
+		await axios.get(url).then((json) => {
+			//받아온 정보값을 dispatch함수로 다시 reducer에 전달
+			dispatch(setYoutube(json.data.items));
 			setLoading(true);
 		});
-	}, []);	
+	};
+
+	useEffect(() => {
+		fetchYoutube();
+	}, []);
 
 	return (
 		<>
-			<Layout name={'Youtube'}>				
-				
-				{items.map((item, idx) => {
+			<Layout name={'Youtube'}>
+				{/* reducer를 통해 store로 부터 전달받은 vidData로 리스트 출력 */}
+				{vidData.map((item, idx) => {
 					const desc = item.snippet.description;
 					const date = item.snippet.publishedAt;
 
-					return (				
-						<article key={idx} onClick={()=>{						
-							setIndex(idx);
-							//자식컴포넌트에서 리턴하고 있는 open함수를 호출해서 팝업 출력
-							pop.current.open();
-						}}>
+					return (
+						<article
+							key={idx}
+							onClick={() => {
+								setIndex(idx);
+								pop.current.open();
+							}}>
 							<img src={item.snippet.thumbnails.medium.url} />
 							<h2>{item.snippet.title}</h2>
 							<p>{desc.length > 150 ? desc.substr(0, 150) + '...' : desc}</p>
@@ -45,18 +56,19 @@ function Youtube() {
 						</article>
 					);
 				})}
-			</Layout>			
-			
-			{/* forwardRef로 내보내진 컴포넌트를 활용하기 위해서는 부모컴포넌트에서 useRef로 참조 */}
-			<Popup ref={pop}>	
-				{/*loading이 true일떄 팝업안에 유튜브 데이터 출력*/}
+			</Layout>
+
+			<Popup ref={pop}>
 				{loading && (
-					<iframe src={'https://www.youtube.com/embed/'+items[index].snippet.resourceId.videoId} frameBorder="0"></iframe>
-				)}	
-				{/* pop.current(Popup컴포넌트가 리턴하는 open, close를 품고 있는 객체) */}
-				<span onClick={()=>pop.current.close()}>close</span>
+					<iframe
+						src={
+							'https://www.youtube.com/embed/' +
+							vidData[index].snippet.resourceId.videoId
+						}
+						frameBorder='0'></iframe>
+				)}
+				<span onClick={() => pop.current.close()}>close</span>
 			</Popup>
-		
 		</>
 	);
 }
