09abb9e7f1c9581698a376e43ee8c70bdc26f245
tubi55 <tubi55@nate.com>
2022-04-21 15:42:46 +0900

    메인페이지 vids컴포넌트에서 미리보기 유튜브 썸네일 클릭시 팝업호출

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,22 +1,45 @@
+import { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
+import Popup from '../common/Popup';
 
 function Vids() {
 	const vidData = useSelector((state) => state.youtubeReducer.youtube);
+	const pop = useRef(null);
+	const [index, setIndex] = useState(0);
 
 	return (
-		<section id='vids' className='myScroll'>
-			<h1>Recent Youtube</h1>
-			<ul className='vidList'>
-				{vidData.map((vid, idx) => {
-					if (idx < 3)
-						return (
-							<li key={idx}>
-								<img src={vid.snippet.thumbnails.medium.url} />
-							</li>
-						);
-				})}
-			</ul>
-		</section>
+		<>
+			<section id='vids' className='myScroll'>
+				<h1>Recent Youtube</h1>
+				<ul className='vidList'>
+					{vidData.map((vid, idx) => {
+						if (idx < 3)
+							return (
+								<li
+									key={idx}
+									onClick={() => {
+										setIndex(idx);
+										pop.current.open();
+									}}>
+									<img src={vid.snippet.thumbnails.medium.url} />
+								</li>
+							);
+					})}
+				</ul>
+			</section>
+
+			<Popup ref={pop}>
+				{vidData.length !== 0 && (
+					<iframe
+						src={
+							'https://www.youtube.com/embed/' +
+							vidData[index].snippet.resourceId.videoId
+						}
+						frameBorder='0'></iframe>
+				)}
+				<span onClick={() => pop.current.close()}>close</span>
+			</Popup>
+		</>
 	);
 }
 
