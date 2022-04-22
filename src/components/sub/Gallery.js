0bb8d1df807cc9eee0432c9976e5b5a73ab47dbc
tubi55 <tubi55@nate.com>
2022-04-22 11:31:37 +0900

    검색 기능 추가

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Layout from '../common/Layout';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -7,15 +7,36 @@ function Gallery() {
 	const dispatch = useDispatch();
 
 	const [opt, setOpt] = useState({ type: 'interest' });
+	//input요소 참조
+	const input = useRef(null);
 
+	//opt state값이 변경될때마다 saga에 action객체 전달
 	useEffect(() => {
-		//action객체를 saga.js로 전달
 		dispatch({ type: 'FLICKR_START', opt });
 	}, [opt]);
 
+	//초기화 함수
+	const initGallery = () => {
+		setOpt({ type: 'interest' });
+	};
+
+	//검색함수
+	const searchTag = () => {
+		const tag = input.current.value;
+		setOpt({ type: 'search', tags: tag });
+	};
+
 	return (
 		<Layout name={'Gallery'}>
-			<h2>Flickr Gallery</h2>
+			<button onClick={initGallery}>갤러리 초기화</button>
+
+			{/* inputBox 추가 */}
+			<div className='inputBox'>
+				<input type='text' ref={input} />
+				{/* 검색 버튼 클릭시 검색함수 호출 */}
+				<button onClick={searchTag}>검색</button>
+			</div>
+
 			<ul>
 				{flickr.map((item, idx) => {
 					return (
