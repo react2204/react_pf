75450e98a12ad7a186273aacb7cf548969186f3e
tubi55 <tubi55@nate.com>
2022-04-22 12:08:14 +0900

    예외처리

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,26 +1,23 @@
 import React, { useEffect, useState, useRef } from 'react';
-import Layout from '../common/Layout';
 import { useSelector, useDispatch } from 'react-redux';
+import Layout from '../common/Layout';
 
 function Gallery() {
-	const { flickr } = useSelector((state) => state.flickrReducer);
+	const { flickr, error } = useSelector((state) => state.flickrReducer);
+	console.log(flickr);
+	console.log(error);
 	const dispatch = useDispatch();
-
-	const [opt, setOpt] = useState({ type: 'interest' });
-	//input요소 참조
+	const [opt, setOpt] = useState({ type: 'interest2' });
 	const input = useRef(null);
 
-	//opt state값이 변경될때마다 saga에 action객체 전달
 	useEffect(() => {
 		dispatch({ type: 'FLICKR_START', opt });
 	}, [opt]);
 
-	//초기화 함수
 	const initGallery = () => {
 		setOpt({ type: 'interest' });
 	};
 
-	//검색함수
 	const searchTag = () => {
 		const tag = input.current.value;
 		setOpt({ type: 'search', tags: tag });
@@ -30,10 +27,8 @@ function Gallery() {
 		<Layout name={'Gallery'}>
 			<button onClick={initGallery}>갤러리 초기화</button>
 
-			{/* inputBox 추가 */}
 			<div className='inputBox'>
 				<input type='text' ref={input} />
-				{/* 검색 버튼 클릭시 검색함수 호출 */}
 				<button onClick={searchTag}>검색</button>
 			</div>
 
--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,22 +1,21 @@
 import { takeLatest, all, put, fork, call } from 'redux-saga/effects';
 import { getFlickr } from './api';
 
-//reducer에 미들웨어 적용되어 실행할 함수
 export default function* rootSaga() {
-	//callFlickr함수를 비동기적으로 호출
 	yield all([fork(callFlickr)]);
 }
 
-//요청받은 action타입에 따라 함수 호출
 export function* callFlickr() {
-	//'FLICKR_START'액션타입이 전달되면 두번째 인수의 함수 호출
 	yield takeLatest('FLICKR_START', returnFlickr);
 }
 
-//action타입에 따라 실행될 generate함수
 export function* returnFlickr(action) {
-	//api.js의 getFlickr함수에 두번째 인수로 받은 값을 적용해서 호출
-	const response = yield call(getFlickr, action.opt);
-	//위의 호출결과로 반환된 값을 'FLICKR_SUCCESS'액션타입으로 reducer에 전달
-	yield put({ type: 'FLICKR_SUCCESS', payload: response.data.photos.photo });
+	try {
+		const response = yield call(getFlickr, action.opt);
+		yield put({ type: 'FLICKR_SUCCESS', payload: response.data.photos.photo });
+	} catch (err) {
+		//해당 api호출이 실패했을때 예외처리
+		//에러내용을 reducer에 전달
+		yield put({ type: 'FLICKR_ERROR', payload: err });
+	}
 }
