d610123230058f816e907b94558fbe8c89514e6c
tubi55 <tubi55@nate.com>
2022-04-22 14:47:17 +0900

    메인컴포넌트에서 scroll될때마다 스크롤된 거리값을 state로 관리

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,63 +7,70 @@ import Pics from './Pics';
 import Btns from './Btns';
 import Anime from '../../class/anim.js';
 
-import {useRef, useEffect, useState} from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 const path = process.env.PUBLIC_URL;
 
-function Main() {  
-  const main = useRef(null); 
-  const pos = useRef([]);
-  const [index, setIndex] = useState(0);
-  
-  const getPos = () => {
-    const secs = main.current.querySelectorAll('.myScroll'); 
-    pos.current=[];   
-    for( const sec of secs) pos.current.push(sec.offsetTop);   
-  }
+function Main() {
+	const main = useRef(null);
+	const pos = useRef([]);
+	const [index, setIndex] = useState(0);
+	//현재 스크롤되는 값을 관리할 state추가
+	const [scrolled, setScrolled] = useState(0);
 
-  const activation = () => {
-    const base = -200;
-    let scroll = window.scrollY;
-    const btns = main.current.querySelectorAll('.btns li');
+	const getPos = () => {
+		const secs = main.current.querySelectorAll('.myScroll');
+		pos.current = [];
+		for (const sec of secs) pos.current.push(sec.offsetTop);
+	};
 
-    pos.current.map((pos, idx)=>{
-      if(scroll >= pos+base){
-        for(const btn of btns) btn.classList.remove('on');
-        btns[idx].classList.add('on');
-      }
-    })
+	const activation = () => {
+		const base = -200;
+		let scroll = window.scrollY;
+		//현재 스크롤되는 거는 값을 scrolled state에 저장
+		setScrolled(scroll);
+		const btns = main.current.querySelectorAll('.btns li');
 
-  }
-  
-  useEffect(()=>{   
-    getPos();
-    window.addEventListener('resize', getPos);
-    window.addEventListener('scroll', activation);
-    return () => {
-      window.removeEventListener('resize', getPos);
-      window.removeEventListener('scroll', activation);
-    }
-  },[]);
+		pos.current.map((pos, idx) => {
+			if (scroll >= pos + base) {
+				for (const btn of btns) btn.classList.remove('on');
+				btns[idx].classList.add('on');
+			}
+		});
+	};
 
-  useEffect(()=>{    
-    new Anime(window, {
-      prop: 'scroll',
-      value: pos.current[index],
-      duration: 500
-    })
-  },[index]);
+	useEffect(() => {
+		getPos();
+		window.addEventListener('resize', getPos);
+		window.addEventListener('scroll', activation);
+		return () => {
+			window.removeEventListener('resize', getPos);
+			window.removeEventListener('scroll', activation);
+		};
+	}, []);
 
-  return (
-    <main ref={main}>
-      <Header type={'main'} logoSrc={`${path}/img/logo1.png`} />
-      <Visual />
-      <News />
-      <Vids />
-      <Pics />
-      <Btns setIndex={setIndex} />
-    </main>
-  )
+	useEffect(() => {
+		new Anime(window, {
+			prop: 'scroll',
+			value: pos.current[index],
+			duration: 500,
+		});
+	}, [index]);
+
+	useEffect(() => {
+		console.log(scrolled);
+	}, [scrolled]);
+
+	return (
+		<main ref={main}>
+			<Header type={'main'} logoSrc={`${path}/img/logo1.png`} />
+			<Visual />
+			<News />
+			<Vids />
+			<Pics />
+			<Btns setIndex={setIndex} />
+		</main>
+	);
 }
 
-export default Main
+export default Main;
