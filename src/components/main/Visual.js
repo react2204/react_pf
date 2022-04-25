f0a8fd3536a4fcfe6458f0037a1aded677bfa8cf
tubi55 <tubi55@nate.com>
2022-04-25 14:39:46 +0900

    스와이퍼 슬라이더에 영상추가

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,28 +1,41 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation } from 'swiper';
 import 'swiper/css';
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+const path = process.env.PUBLIC_URL;
 
 function Visual() {
-	return <figure id='visual' className='myScroll'>
-		<Swiper
-			spaceBetween={40}
-			slidesPerView={3}
-			loop={true}
-			pagination={{
-				clickable: true,
-			}}
-			navigation={true}
-			modules={[Pagination, Navigation]}
-		>
-			<SwiperSlide>1</SwiperSlide>
-			<SwiperSlide>2</SwiperSlide>
-			<SwiperSlide>3</SwiperSlide>
-			<SwiperSlide>4</SwiperSlide>
-			<SwiperSlide>5</SwiperSlide>
-		</Swiper>
-	</figure>;
+	return (
+		<figure id='visual' className='myScroll'>
+			<Swiper
+				spaceBetween={40}
+				slidesPerView={3}
+				loop={true}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={true}
+				modules={[Pagination, Navigation]}>
+				<SwiperSlide>
+					<video src={`${path}/img/vid1.mp4`} autoPlay muted loop></video>
+				</SwiperSlide>
+				<SwiperSlide>
+					<video src={`${path}/img/vid2.mp4`} autoPlay muted loop></video>
+				</SwiperSlide>
+				<SwiperSlide>
+					<video src={`${path}/img/vid3.mp4`} autoPlay muted loop></video>
+				</SwiperSlide>
+				<SwiperSlide>
+					<video src={`${path}/img/vid4.mp4`} autoPlay muted loop></video>
+				</SwiperSlide>
+				<SwiperSlide>
+					<video src={`${path}/img/vid5.mp4`} autoPlay muted loop></video>
+				</SwiperSlide>
+			</Swiper>
+		</figure>
+	);
 }
 
 export default Visual;
