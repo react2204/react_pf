b7e058f8cff013897126ac493f2f9b3e742aa123
tubi55 <tubi55@nate.com>
2022-04-27 16:16:47 +0900

    서브 비주얼 영역 벗어나면 좌표값을 구하지 않도록 처리

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,22 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
-const path = process.env.PUBLIC_URL;
+import React, { useEffect, useRef } from 'react';
 
 function Layout(props) {
 	let frame = useRef(null);
 	const cursor = useRef(null);
+	let isCursor = false;
 
 	const handleMove = (e) => {
-		cursor.current.style.left = e.clientX + 'px';
-		cursor.current.style.top = e.clientY + 'px';
+		if (isCursor) {
+			console.log('move');
+			cursor.current.style.left = e.clientX + 'px';
+			cursor.current.style.top = e.clientY + 'px';
+		}
 	};
 
 	useEffect(() => {
 		//서브페이지 비주얼 영역에서 마우스가 벗어나면 커서 사라지게 이벤트 처리
+
 		const figure = frame.current.querySelector('figure');
 		figure.addEventListener('mouseenter', () => {
+			isCursor = true;
 			cursor.current.style.display = 'block';
 		});
 		figure.addEventListener('mouseleave', () => {
+			isCursor = false;
 			cursor.current.style.display = 'none';
 		});
 
